81f05812160ca9d8f057cb2d5cc9925e5f5636f6
Gustavo Silvério Rosa <105025089+GustavoSRos4@users.noreply.github.com>
2024-08-13 23:19:37 +0000

    aula01 13/08/2024 20:21

---
+++ aula01/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program{
+    public static void Main(string[] args){
+        Console.WriteLine("Cadastro de Pessoa!");
+        Console.WriteLine("Digite o nome: ");
+        string? NomeP = Console.ReadLine();
+        Console.WriteLine("Digite a idade: ");
+        int? IdadeP = int.Parse(Console.ReadLine());
+
+
+        Pessoa p1 = new Pessoa(NomeP, IdadeP);
+        Pessoa p2 = new Pessoa("João", 20);
+
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+    }
+}
