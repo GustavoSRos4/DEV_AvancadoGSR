11ba00a418f5859ab93171131d7e76644b62d7dc
Gustavo Silvério Rosa <105025089+GustavoSRos4@users.noreply.github.com>
2024-08-27 23:30:58 +0000

    Atividade exagerada, mais adorei fazer :)

---
+++ aula06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class Program
+{
+    //Delegates
+    public delegate void ExibirNumero(int numero);
+
+
+    //Funções
+    public static void imprimeNumero(int numero)
+    {
+        Console.WriteLine("O Numero é : " + numero);
+    }
+
+    public static void ExibirQuadrado(int numero)
+    {
+        Console.WriteLine("O Quadrado do numero é : " + numero * numero);
+    }
+    //main
+    public static void Main()
+    {
+        ExibirNumero delegateTeste = imprimeNumero;
+        delegateTeste += ExibirQuadrado;
+
+        delegateTeste += delegate (int numero)
+        {
+            Console.WriteLine("O Cubo do numero é : " + numero * numero * numero);
+        };
+
+        delegateTeste(10);
+
+    }
+
+}
