bdd2ed3c47e9408e7c10df566dda87d1ce03fae3
Gustavo Silvério Rosa <105025089+GustavoSRos4@users.noreply.github.com>
2024-08-27 00:29:55 +0000

    Aula05 files upload

---
+++ aula05/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main(String[] args)
+    {
+        //faça uma conversao de dados explicita e outra implicita e uma usando o Convert, e imprima o resultado de cada um
+        //conversao implicita
+        int a = 10;
+        double b = a;
+        Console.WriteLine(b);
+        //conversao explicita
+        double c = 10.5;
+        int d = (int)c;
+        Console.WriteLine(d);
+        //conversao com Convert
+        string e = "10";
+        int f = Convert.ToInt32(e);
+        Console.WriteLine(f);
+    
+    }
+}
