8d1207de871cbb45fe1b02f21b32bf944213a55a
GustavoSRos4 <gustavorosasilverio@gmail.com>
2024-09-23 21:36:23 -0300

    EntityFramework and Data Persistence

--- aula05/obj/Debug/net8.0/aula05.AssemblyInfo.cs
+++ aula05/obj/Debug/net8.0/aula05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ba00a418f5859ab93171131d7e76644b62d7dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b0c09020a7d53cf3bace6f2af23fa7d2e7e6e2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ aula10_EF/Program.cs
@@ -0,0 +1,26 @@
+﻿using aula10_EF.Models;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        using (var context = new PessoaContext())
+        {
+            //Criar datavase se nao existir!!
+            context.Database.EnsureCreated();
+
+            //ADD nova pessoa
+            var pessoa1 = new Pessoa { Nome = "Gustavo", Idade = 20 };
+
+            context.Pessoas.Add(pessoa1);
+            context.SaveChanges();
+
+            //Listar as pessoas cadastradas
+            var listaPessoas = context.Pessoas.ToList();
+            foreach (var p in listaPessoas)
+            {
+                Console.WriteLine($"Nome: {p.Nome}, Idade: {p.Idade}");
+            }
+        }
+    }
+}
